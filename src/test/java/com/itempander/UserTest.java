ab51ce417dcae63b9dd7aca3f0e40f062280cf69
top <dks2922@gmail.com>
2023-05-07 19:14:49 +0900

    TEST : UserTest Add

---
+++ src/test/java/com/itempander/UserTest.java
@@ -0,0 +1,12 @@
+package com.itempander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserTest {
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
