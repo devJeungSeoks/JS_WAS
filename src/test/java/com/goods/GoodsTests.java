0bee9726abf64bd2dd8e4f48415880f70d58157a
JinSeokAn <dkswlstjr000@naver.com>
2023-05-15 23:06:22 +0900

    Test : 상품등록 추가

--- src/test/java/com/goods/GoodsTests.java
+++ src/test/java/com/goods/GoodsTests.java
@@ -1,5 +1,7 @@
 package com.goods;
 
+import com.goods.controller.GoodsController;
+import com.goods.dto.GoodsDTO;
 import com.goods.repository.GoodsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +14,28 @@ public class GoodsTests {
     @Autowired
     GoodsRepository goodsRepository;
 
+    @Autowired
+    GoodsController goodsController;
 
     @Test
     public void test(){
         List goods = goodsRepository.findAll();
-
         System.out.println("list : " + goods);
+    }
+    @Test
+    public void createTest(){
 
-//        return list;
+        GoodsDTO goodsDTO = new GoodsDTO();
+        goodsDTO.setContent("TEST");
+        goodsDTO.setProductName("TEST");
+        goodsDTO.setKind("1");
+        goodsDTO.setCostPrice(20);
+        goodsDTO.setSalesPrice(20);
+        goodsDTO.setProfitPrice(20);
+        goodsDTO.setContent("TEST입니다.");
+        goodsDTO.setImage(null);
+        goodsDTO.setUseYn("Y");
+        System.out.println("goodsDTO : " + goodsDTO);
+        goodsController.createGoods(goodsDTO);
     }
 }
