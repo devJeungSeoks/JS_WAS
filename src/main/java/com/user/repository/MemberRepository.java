5d69d845b3c898479f8e25d267007e8d14b75de3
JinSeokAn <dkswlstjr000@naver.com>
2023-05-21 22:13:19 +0900

    feat : Repository (Integer -> Long) 타입 변경

--- src/main/java/com/user/repository/MemberRepository.java
+++ src/main/java/com/user/repository/MemberRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-//    UserMember findByUserId(String userId);
+    Member findByMemberId(String memberId);
+
 }
