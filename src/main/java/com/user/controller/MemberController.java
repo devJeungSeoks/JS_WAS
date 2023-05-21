e9f95660290646b61799232a73699d38c91b759d
JinSeokAn <dkswlstjr000@naver.com>
2023-05-21 22:12:47 +0900

    feat : 회원 로그인 추가(추후에 Security 적용한 권한 인가 추가)

--- src/main/java/com/user/controller/MemberController.java
+++ src/main/java/com/user/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.user.controller;
 
 import com.user.dto.MemberDTO;
+import com.user.entity.Member;
+import com.user.entity.MemberDetail;
 import com.user.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,19 @@ public class MemberController {
     @PostMapping("/signUp")
     public ApiResponse signUp(@RequestBody MemberDTO memberDTO) {
         memberService.signUp(memberDTO);
-        return new ApiResponse(true,"회원가입이 완료되었습니다.");
+        return new ApiResponse(true, "회원가입이 완료되었습니다.");
+    }
+
+    @PostMapping("/login")
+    public MemberDetail login(@RequestBody MemberDTO memberDTO) {
+        Member member = memberService.login(memberDTO);
+
+        if (member != null) {
+            memberDTO.setNo(member.getNo());
+            MemberDetail memberDetail = memberService.memberDetailSelect(memberDTO.getNo());
+            return memberDetail;
+        } else {
+            return null;
+        }
     }
 }
