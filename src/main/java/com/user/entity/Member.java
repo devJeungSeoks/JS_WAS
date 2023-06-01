3dc8bdbd74c4908e9b89f5bfb8df1653600f5ce4
JinSeokAn <dkswlstjr000@naver.com>
2023-06-01 22:22:26 +0900

    feat : Member Entity (adminYn, saveStatus) 필드 추가

--- src/main/java/com/user/entity/Member.java
+++ src/main/java/com/user/entity/Member.java
@@ -8,8 +8,14 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "member")
-@Getter @Setter
+@Table(name = "member"
+        , uniqueConstraints = {
+        @UniqueConstraint(columnNames = "member_no", name = "memberNo_pk"),
+        @UniqueConstraint(columnNames = "memberId", name = "memberId_pk")
+    }
+)
+@Getter
+@Setter
 public class Member extends BaseEntity {
 
     @Id
@@ -23,9 +29,9 @@ public class Member extends BaseEntity {
     @Column(columnDefinition = "varchar(255) not null comment '패스워드'")
     private String password;
 
-    @Column(columnDefinition = "varchar(1) not null comment '관리자여부'")
+    @Column(columnDefinition = "varchar(1) default 'N' not null comment '관리자여부'")
     private String adminYn;
-    @Column(columnDefinition = "varchar(1) not null comment '활성여부'")
+    @Column(columnDefinition = "varchar(1) default 'Y' not null comment '활성여부'")
     private String saveStatus;
 
     @JsonManagedReference
