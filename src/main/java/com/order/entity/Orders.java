811bce06c0e759654a4905561c4daeaa20ad677d
JinSeokAn <dkswlstjr000@naver.com>
2023-06-11 16:15:33 +0900

    feat : 주문 Entity 추가

---
+++ src/main/java/com/order/entity/Orders.java
@@ -0,0 +1,44 @@
+package com.order.entity;
+
+import com.common.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.user.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"orderNo"}, name = "orderNo")
+        })
+@Getter
+@Setter
+public class Orders extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "order_seq")
+    private Long no;
+
+    @Column(columnDefinition = "varchar(20) not null comment '주문번호'")
+    private String orderNo;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberNo")
+    private Member member;
+
+    @Column(columnDefinition = "varchar(50) not null comment '주문자 이메일'")
+    private String memberEmail;
+
+    @Column(columnDefinition = "varchar(20) not null comment '주문자 번호'")
+    private String memberPhone;
+
+    @Column(columnDefinition = "varchar(20) not null comment '상품코드'")
+    private String gShortCode;
+
+    @Column(columnDefinition = "varchar(1) default '1' not null comment '주문상태(1: 대기, 2: 주문승인, 3: 결제대기, 4: 배송대기, 5:배송 준비중, 6: 배송중, 7: 배송완료, 8: 결제완료)'")
+    private String orderLevel;
+}
+
