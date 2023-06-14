db8c2f49386583a6ce7bedf54485b3ed0e9a06a4
JinSeokAn <dkswlstjr000@naver.com>
2023-06-14 21:25:10 +0900

    Test : 주문등록 junit 추가.

---
+++ src/test/java/com/orders/OrdersTests.java
@@ -0,0 +1,36 @@
+package com.orders;
+
+import com.order.dto.OrderDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootTest
+public class OrdersTests {
+    @Test
+    public void createTest(){
+        OrderDTO orderDTO = new OrderDTO();
+        System.out.println("toString : " + orderDTO.toString());
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+        String formatDateTime = now.format(formatter);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("O");
+        stringBuilder.append(formatDateTime);
+
+        orderDTO.setOrderNo(stringBuilder.toString());
+        orderDTO.setMemberNo(1L);
+        orderDTO.setMemberEmail("dks2922@naver.com");
+        orderDTO.setMemberPhone("01034340069");
+        orderDTO.setGoodsCode("2058");
+        orderDTO.setOrderLevel("TEST입니다.");
+
+
+        System.out.println("orderDTO : " + orderDTO);
+
+    }
+}
