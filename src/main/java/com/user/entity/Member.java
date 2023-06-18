d046cc9365b56dd6867fa346dcf83fb1e61789a2
JinSeokAn <dkswlstjr000@naver.com>
2023-06-18 14:30:38 +0900

    Refactor : 상품, 회원, 회원상세, 주문 Entity (ColumnComment, ColumnDefault) 어노테이션 추가.

--- src/main/java/com/user/entity/Member.java
+++ src/main/java/com/user/entity/Member.java
@@ -4,28 +4,35 @@ import com.common.entity.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "member")
-@Getter @Setter
+@Getter
+@Setter
 public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "member_seq")
-    @Column(name = "member_no", columnDefinition = "int not null comment '회원 고유번호'")
+    @Column(nullable = false, name = "member_no")
+    @Comment("회원 고유번호")
     private Long no;
 
-    @Column(columnDefinition = "varchar(20) not null comment '아이디'")
+    @Column(length = 20, unique = true, nullable = false)
+    @Comment("아이디")
     private String memberId;
 
-    @Column(columnDefinition = "varchar(255) not null comment '패스워드'")
+    @Column(length = 255, nullable = false)
+    @Comment("패스워드")
     private String password;
 
-    @Column(columnDefinition = "varchar(1) default 'N' not null comment '관리자여부'")
+    @Column(length = 1, nullable = false)
+    @Comment("관리자여부")
     private String adminYn;
-    @Column(columnDefinition = "varchar(1) default 'Y' not null comment '활성여부'")
+    @Column(length = 1, nullable = false)
+    @Comment("활성여부")
     private String saveStatus;
 
     @JsonManagedReference
